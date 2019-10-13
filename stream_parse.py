6f905d191277884b262e7d2691648466edee0a0e
WardHills <mail@wardhills.net>
2019-10-13 18:30:19 +0100

    some refactoring and completed the write to file and simple screen display of all received data.  Not revceiving TLS data from PITS.

--- stream_parse.py
+++ stream_parse.py
@@ -23,62 +23,34 @@ headings = ['callsign',
             'Satellites',
             'Temperature',
             'BatteryVoltage',
-            'BatteryCurrent'
+            'BatteryCurrent',
+            'accel_x',
+            'accel_y',
+            'accel_Z',
+            'mag_x',
+            'mag_y',
+            'mag_Z',
+            'G-X',
+            'G-Y',
+            'G-Z',
+            'Temp',
+            'Humid',
+            'TimeStamp'
             ]
 
-def get_log_name(fname='fldigi'):
-    now = datetime.datetime.now()
-    today = now.strftime("%Y%m%d")
-    return fname+today+'.log'
-
-def get_last_lines(file):
-    with open(file, "rb") as f:
-        #first = f.readline()  # Read the first line.
-        f.seek(-2, os.SEEK_END)  # Jump to the second last byte.
-        while f.read(1) != b"\n":  # Until EOL is found...
-            f.seek(-3, os.SEEK_CUR)
-#            f.seek(-2, os.SEEK_CUR)  # ...jump back the read byte plus one more.
-        last = f.readline()  # Read last line.
-    return last
-
-def extract_sentance(line, sep='$$'):
-    try:
-        if sep in last_line:
-            extract = last_line.split('$$', 50)
-            #print('Extracted', extract[1])
-            return extract[1]
-        elif sep not in last_line:
-            #print('not found')
-            pass
-    except Exception as e:
-        print(e)
-        pass
-    return
-
-def make_list(extracted):
-    try:
-        if not extracted == None:
-            made = extracted.split(',')
-        #made = extracted.Split(',').ToList()
-            return made
-
-    except Exception as e:
-        print(e)
-        pass
-
-def write_file(list, fname='sentancelog',headings=headings):
+def write_file(list, fname='data',headings=headings,path='./'):
     now = datetime.datetime.now()
     today = now.strftime("%Y%m%d")
 
     # TODO make method for constructing the full path with file type exention
     full_name = fname+today+'.csv'
-    full_path = log_path+full_name
+    full_path = path+full_name
 
     try:
         if os.path.isfile(full_path):
             with open(full_path, 'a', newline='') as f:
                 wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-                wr.writerow(list)
+                wr.writerow(list)  
             return
 
         with open(full_path, 'w', newline='') as f:
@@ -95,20 +67,38 @@ def write_file(list, fname='sentancelog',headings=headings):
 ##Code below from https://github.com/PiInTheSky/LCARS (with thanks)
 
 def ProcessdlfldigiLine(line):
-    field_list = line.split(",")
 
-    print(field_list)
+    try:
+  
+        data_list = line.split(",")
+        #print('data_list[0] = ', data_list[0])
+        #print('$'+call_sign)
 
-    targ_sentance = field_list
+        if data_list[0] == '$'+call_sign:
+            time_item = data_list[2]
+            time_stamp = str(data_list[-1])             # extract the last field, convet to str
+            time_stamp = time_stamp.split('*')[0]       # split chars appened to by PITS software
+            
+            data_list.pop(0)                            # remove the strings from the list ( call sign)  
+            data_list.pop(1)                            #        transmit time
+            data_list.pop(-1)                           #        timestamp+appended str  
+            data_list.append(float(time_stamp))         # add flast part of timestamp back into list  
 
-    targ_pos = [
-        float(targ_sentance[3]),
-        float(targ_sentance[4]),
-        float(targ_sentance[5]),
-        targ_sentance[0]
-    ]
-    
-    calc_vector(targ_pos, my_pos)
+            data = [float(x) for x in data_list] # converts strings to float
+
+            data.insert(0,'$'+call_sign)         # re add the str data that was removed above
+            data.insert(2,time_item)               
+
+            calc_vector(data, base_pos)
+            
+            write_file(data)
+
+            report_status(data)
+
+    except Exception as e:
+        print('error in ProcessdlfldigiLine')
+        print(e)
+        pass
 
 def Processdlfldigi(s):
     line = ''
@@ -128,7 +118,6 @@ def Processdlfldigi(s):
                     line = temp
                 elif line != '':
                     line = line + temp
-
         else:
             time.sleep(1)
 
@@ -148,9 +137,11 @@ def dodlfldigi(host, port):
 
         s.close()
 
-    except:
-        # Sources[2]['connected'] = 0
+    except Exception as e:
+        print('Error in dodlfldigi')
+        print(e)
         pass
+   
 
 
 def dlfldigi_thread():
@@ -162,42 +153,96 @@ def dlfldigi_thread():
     while 1:
         dodlfldigi(host, port)
 
-def calc_vector(targ_pos, my_pos):
-    #  tar_pos _ lat_B, lon_B, alt_B , 'name'
-    print( 'calc vector')
-    print(targ_pos[0])
+def calc_vector(targ_sentance, base_pos):
+    try:
+                                                    #targ_sentance[3] - Lat
+                                                    #targ_sentance[4] - Lon
+                                                    #targ_sentance[5] - Alt
 
-   # p1 = GeoPoint(lat=lat_A, lon=lon_A, altitude=alt_A, name=name_A)
-    p1 = GeoPoint(targ_pos[0],targ_pos[1],targ_pos[2],targ_pos[3])
-    print(p1)
-    #p2 = GeoPoint(lat=lat_B, lon=lon_B, altitude=alt_B, name=name_B)
-    p2 = GeoPoint(my_pos[0],my_pos[1],my_pos[2],my_pos[3])
+        if 45 <= int(targ_sentance[3]) <= 70:       # is Lat is a reasonable number, for Europe. 
+                                                    # extract the position (lat,lon alt and name)
+            targ_pos = [
+                float(targ_sentance[3]),
+                float(targ_sentance[4]),
+                float(targ_sentance[5]),
+                targ_sentance[0]
+                ]
 
-    connection_vector = p2 - p1
+        targ = GeoPoint(targ_pos[0],targ_pos[1],targ_pos[2],targ_pos[3])
 
-    print('connection_vector' , connection_vector)
+        base = GeoPoint(base_pos[0],base_pos[1],base_pos[2],base_pos[3])
 
-    GeonumDistance = connection_vector.magnitude
-    GeonumDistance = float("{0:.4f}".format(GeonumDistance))
+        connection_vector = targ - base
+        print('\n=Starts=======================================================\n')
+        print('Reveived at: ',datetime.datetime.now())
+        print('Target: ', targ_pos[3],' , lat/lon : ',targ_pos[0], ' / ',targ_pos[1], 'Alt : ',targ_pos[2])  
+        print('\n')
+        print('connection_vector' , connection_vector)
 
-    print("Geonum:", GeonumDistance, "km")
-    return  GeonumDistance
+        GeonumDistance = connection_vector.magnitude
+        #GeonumDistance = float("{0:.4f}".format(GeonumDistance))
+        
+        print("Geonum:", GeonumDistance, "km")
+        print("--")
+        return  GeonumDistance
+    
+    except Exception as e:
+        print('error in calc_vector')
+        print(e)
+        pass
 
+def report_status(data, headings=headings):
+    d = dict(zip(headings, data))
+   # print(d)
+    print('Horizontal (m/s, heading) :',
+          d['HorizontalSpeed'],
+          d['Heading'],
+          '\n')
+    print('Temp. (internal)', d['Temperature'],
+          '\n')
+    print('Battery V,mA ',
+          d['BatteryVoltage'],
+          d['BatteryCurrent'],
+          '\n')
+
+    print('Acceleration (x,y,z) m/s2 :',
+          d['accel_x'],
+          d['accel_y'],
+          d['accel_Z'],
+          '\n')
+
+    print ('Magnetic field (x,y,z) gauss (??UNITS??) :',
+           d['mag_x'],
+           d['mag_y'],
+           d['mag_Z'],
+          '\n')
+    
+    print ('Gyro (x,y,z) rad/s  :',
+           d['G-X'],
+           d['G-Y'],
+           d['G-Z'],
+          '\n')
+
+    print('Temp/Humid (external) :',
+          d['Temp'],
+          d['Humid'],
+          '\n')
+
+    print('TimeStamp (time at target) :', datetime.datetime.fromtimestamp(d['TimeStamp']).isoformat())
+    print('\n================================================================ends\n')
 
 if __name__ == '__main__':
 
-    global targ_sentance
-    global my_pos
+    #global targ_sentance
+    global base_pos
+    global call_sign
 
-    my_pos = [52.2265, 0.0901, 24, 'Albion House']    #lat_B, lon_B, alt_B , 'name'
+    call_sign = 'CST'
+
+    base_pos = [52.2265, 0.0901, 24, 'Albion House']    #lat_B, lon_B, alt_B , 'name'
 
     dlfldigi_thread()
     dodlfldigi()
 
-    targ_pos = [
-        targ_sentance[4],
-        targ_sentance[5],
-        targ_sentance[6],
-        targ_sentance[0]
-        ]
-    print(targ_pos)
+
+
